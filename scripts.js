34026b7bb0f52078304d234a90e69c157f053d69
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-08 15:28:07 +0200

    Implement reducer to handle tally app state changes

--- scripts.js
+++ scripts.js
@@ -9,3 +9,16 @@ const subtractTally = { type: 'SUBTRACT' };
 // Resets the count to its initial value
 const resetTally = { type: 'RESET' };
 
+/* Reducer function to manage the state of the tally app */
+const tallyReducer  = (state = tallyState, action) => {
+    switch (action.type) {
+        case 'ADD':
+            return { ...state, count: state.count + 1 };
+        case 'SUBTRACT':
+            return { ...state, count: state.count - 1 };
+        case 'RESET':
+            return { ...state, count: 0 };
+        default:
+            return state;
+    }
+};
